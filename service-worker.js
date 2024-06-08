6009162d2103421c4e820ad39e676d6204d2199f
MrBreakNFix <73456238+MrBreakNFix@users.noreply.github.com>
2024-06-08 15:33:33 -0700

    why not

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,25 @@
-// Function to display a notification
-function displayNotification() {
-    self.registration.showNotification('Notification Title', {
-        body: 'This is a notification sent by a service worker!'
-    });
+// Function to display a notification with clipboard contents
+function displayClipboardNotification(content) {
+    // Check if notification permission is granted
+    if (Notification.permission === 'granted') {
+        self.registration.showNotification('Clipboard Notification', {
+            body: content,
+        });
+    }
 }
 
-// Schedule notification
-function scheduleNotification() {
-    setInterval(() => {
-        displayNotification();
-    }, 15000);
-}
-
-self.addEventListener('install', event => {
-    console.log('Service worker installed');
-    scheduleNotification();
+// Listen for messages from the main page
+self.addEventListener('message', event => {
+    const clipboardText = event.data.clipboardText;
+    if (clipboardText) {
+        displayClipboardNotification(clipboardText);
+    }
 });
 
+// Add event listener for notification click
 self.addEventListener('notificationclick', event => {
+    // Handle notification click event here
     console.log('Notification clicked');
+    // Close the notification
     event.notification.close();
 });
--- utils.js
+++
@@ -1,4 +0,0 @@
-// utils.js
-function redirectToLogin() {
-    window.location.href = 'https://login.live.com/oauth20_authorize.srf?client_id=c56ecfc5-eac9-439a-99ac-f8d7e29c07fa&response_type=code&redirect_uri=https://mrbreaknfix.com/mcauth&scope=XboxLive.signin%20offline_access';
-}
