7315a5a0c91ceff416cd1eee40aafc6f631214bc
MrBreakNFix <73456238+MrBreakNFix@users.noreply.github.com>
2024-07-13 23:23:12 -0700

    Prep for msft auth scheme

--- api.js
+++ api.js
@@ -3,6 +3,7 @@
 var apiBaseUrl = 'https://script.google.com/macros/s/AKfycbyELpW9nBFh8nvBJhOAX3Lgxrv9CwOXG0ifvzds6mw4H1luaZvNtRx7zH-o2FuSfj4H5A/exec';
 // dang im here again, i still have no recollection of making this file lol :)
 // bro what?? i never remember making this file
+// what? I still don't remember ever making this file???
 
 function makeApiCall(paramsObject, callbackName) {
     var script = document.createElement('script');
